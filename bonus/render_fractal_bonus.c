7a75b9773d15c7aeb7fa0256ea2395302d1938c3
bede-car <bede-car@student.42sp.org.br>
2023-01-22 14:12:19 -0300

    cores bonus versão final

--- bonus/render_fractal_bonus.c
+++ bonus/render_fractal_bonus.c
@@ -6,7 +6,7 @@
 /*   By: bede-car <bede-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 15:17:49 by bede-car          #+#    #+#             */
-/*   Updated: 2023/01/22 12:12:49 by bede-car         ###   ########.fr       */
+/*   Updated: 2023/01/22 14:11:23 by bede-car         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,21 +55,21 @@ int	change_color(t_math *math)
 	math->fractal(math);
 	if (math->color == 1)
 	{
-	rgb[0] = sin(0.05 * math->interations + 58) * 126 + 127;
-	rgb[1] = sin(0.05 * math->interations + 4) * 126 + 127;
-	rgb[2] = sin(0.05 * math->interations + 100) * 126 + 127;
+	rgb[0] = sin(0.1 * math->interations + 5) * 125 + 126;
+	rgb[1] = sin(0.1 * math->interations + 6) * 125 + 126;
+	rgb[2] = sin(0.1 * math->interations + 7) * 125 + 126;
 	}
 	else if (math->color == 2)
 	{
-	rgb[0] = sin(0.1 * math->interations + 100) * 120 + 126;
-	rgb[1] = sin(0.1 * math->interations + 6) * 120 + 126;
-	rgb[2] = sin(0.1 * math->interations + 70) * 120 + 126;
+	rgb[0] = sin(0.1 * math->interations + 58) * 126 + 127;
+	rgb[1] = sin(0.1 * math->interations + 10) * 126 + 127;
+	rgb[2] = sin(0.1 * math->interations + 100) * 126 + 127;
 	}
 	else if (math->color == 3)
 	{
-	rgb[0] = sin(0.05 * math->interations + 50) * 127 + 128;
-	rgb[1] = sin(0.05 * math->interations + 30) * 127 + 128;
-	rgb[2] = sin(0.05 * math->interations + 1) * 127 + 128;
+	rgb[0] = sin(0.05 * math->interations + 70) * 124 + 125;
+	rgb[1] = sin(0.05 * math->interations + 100) * 124 + 125;
+	rgb[2] = sin(0.05 * math->interations + 500) * 124 + 125;
 	}
 	return (rgb[0] << 16 | rgb[1] << 8 | rgb[2]);
 }
